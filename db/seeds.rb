2bffc6a71da0192395887faa82dc8c4b34c779ae
belzunces <lcbelzunces@gmail.com>
2019-11-12 10:30:02 +0100

    a ne pas valider

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "cleaning database"
+
+Event.destroy_all
+100.times do
+ event = Event.new(
+   name: Faker::Company.name,
+   date: Faker::Date.forward(300),
+   location: Faker::Address.city,
+   periodicity: ['mois', 'année', 'jour'].sample,
+   category: ['concert', 'theatre', 'dancing'].sample,
+   sub_category: ['humour', 'drame', 'chanson', 'art de rue', 'aniamtion'].sample
+   )
+   event.save!
+ end
+
+ puts "finished"
