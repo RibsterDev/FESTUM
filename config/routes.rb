cc69a546bfab263bdd23f9b7ee600087e903ab5a
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-12 15:03:08 +0100

    ajout def catégory au event controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
-# class EventsController < ApplicationController
-#   def cookies
-#     date = Events.where(date_start: cookies[:date_start])
-#     loca = Event.where(location: cookies[:location])
-
-#   end
-# end
 
