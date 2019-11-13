87e2818f183524e47cce53a523915df982ce6662
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-13 11:50:03 +0100

    gestion des dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,18 +51,80 @@ user3.save!
 users = [user1, user2, user3]
 
 
-10.times do
+# 10.times do
+#   event = Event.new(
+#     name: Faker::Company.name,
+#     date_start: Faker::Date.forward(days:300),
+#     location: Faker::Address.city,
+#     periodicity: ['mois', 'année', 'jour'].sample,
+#     category: ['concert', 'theatre', 'dancing'].sample,
+#     sub_category: ['humour', 'drame', 'chanson', 'art de rue', 'animation'].sample,
+#     creator: users.sample
+#     )
+#   event.save!
+# end
+
   event = Event.new(
-    name: Faker::Company.name,
-    date_start: Faker::Date.forward(days:300),
-    location: Faker::Address.city,
+    name: "teuf1",
+    date_start: Date.new(2019, 11, 01),
+    date_end: Date.new(2019, 11, 30),
+    location: "Lyon",
     periodicity: ['mois', 'année', 'jour'].sample,
-    category: ['concert', 'theatre', 'dancing'].sample,
-    sub_category: ['humour', 'drame', 'chanson', 'art de rue', 'animation'].sample,
+    category: "concert",
+    sub_category: 'humour',
     creator: users.sample
     )
   event.save!
-end
+
+  event = Event.new(
+    name: "teuf2",
+    date_start: Date.new(2019, 11, 14),
+    date_end: Date.new(2019, 11, 30),
+    location: "Lyon",
+    periodicity: ['mois', 'année', 'jour'].sample,
+    category: "dancing",
+    sub_category: 'drame',
+    creator: users.sample
+    )
+  event.save!
+
+    event = Event.new(
+    name: "teuf3",
+    date_start: Date.new(2019, 11, 01),
+    date_end: Date.new(2019, 11, 10),
+    location: "Lyon",
+    periodicity: ['mois', 'année', 'jour'].sample,
+    category: 'theatre',
+    sub_category: 'art de rue',
+    creator: users.sample
+    )
+  event.save!
+
+      event = Event.new(
+    name: "teuf4",
+    date_start: Date.new(2019, 11, 01),
+    date_end: Date.new(2019, 11, 15),
+    location: "Lyon",
+    periodicity: ['mois', 'année', 'jour'].sample,
+    category: 'concert',
+    sub_category: 'art de rue',
+    creator: users.sample
+    )
+  event.save!
+
+      event = Event.new(
+    name: "teuf5",
+    date_start: Date.new(2019, 11, 12),
+    date_end: Date.new(2019, 11, 17),
+    location: "Lyon",
+    periodicity: ['mois', 'année', 'jour'].sample,
+    category: 'theatre',
+    sub_category: 'art de rue',
+    creator: users.sample
+    )
+  event.save!
+
+
 
 
 puts "finished"
