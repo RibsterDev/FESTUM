58d7bffb73833d8918ddb4ceb9f5cf7aa99372f0
belzunces <lcbelzunces@gmail.com>
2019-11-14 17:32:59 +0100

    probleme de route fixe, ajout d'une route, probleme de controller fixé

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,11 +1,12 @@
 class EventUsersController < ApplicationController
   def index
-    @event_users = EventUser.all
+    @events = current_user.events
   end
 
   def create
     # bien prende l'event_id de l'url en fonction de la route
-    event = Event.find(params[:event_id])
+    # event = Event.find(params[:event_id])
+    event = Event.find(cookies[:event_id])
     # EventUser.new à belongs to user , event d'ou les clés user & event
     @event_user = EventUser.new(user: current_user, event: event)
     @event_user.save!
@@ -19,6 +20,5 @@ class EventUsersController < ApplicationController
     @event_user.destroy
     # redirect vers la vue de mes events à moi index de event_users
     redirect_to event_users_path
-
   end
 end
