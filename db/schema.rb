6822ad211eb6eb85e6f59c2a0811fd7099adab93
Evan Wichlarz <evanwichlarz1@hotmail.fr>
2019-11-14 14:11:14 +0100

    upload de photo & carrewave & view mylist index show avec photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_144959) do
+ActiveRecord::Schema.define(version: 2019_11_14_123804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_144959) do
     t.bigint "creator_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
