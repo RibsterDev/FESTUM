e2bad3d3a2b1d410f0dfde36bee67a79a5cc0db8
belzunces <lcbelzunces@gmail.com>
2019-11-15 15:04:36 +0100

    footer ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_100401) do
+ActiveRecord::Schema.define(version: 2019_11_15_111944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
