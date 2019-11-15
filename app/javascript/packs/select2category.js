29ecf0bd2bdad82abd7053756f80b4a73dd2bb65
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-15 15:52:33 +0100

    fix compilation assets

--- app/javascript/packs/select2category.js
+++ app/javascript/packs/select2category.js
@@ -1,19 +1,19 @@
-import $ from 'jquery';
-import select2 from 'select2/dist/js/select2';
+// import $ from 'jquery';
+// import select2 from 'select2/dist/js/select2';
 
 
 
-const categories = ["Théatre", "Musique", "Cinéma"];
+// const categories = ["Théatre", "Musique", "Cinéma"];
 
-const initSelect2 = () => {
-  // Turn on js-selectable class so that it becomes SELCT 2 tag
-  $('.js-searchable').select2({
-    theme: "bootstrap",
-    data: categories,
-    width: '100%'
-    // If you are using Bootstrap, please add　`theme: "bootstrap"` too.
-  });
-};
+// const initSelect2 = () => {
+//   // Turn on js-selectable class so that it becomes SELCT 2 tag
+//   $('.js-searchable').select2({
+//     theme: "bootstrap",
+//     data: categories,
+//     width: '100%'
+//     // If you are using Bootstrap, please add　`theme: "bootstrap"` too.
+//   });
+// };
 
-export { initSelect2 };
+// export { initSelect2 };
 
