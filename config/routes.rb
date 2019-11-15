3bd363328de9b46a496a9bd8569b675873dbdaa2
Nicolas Morlot <nicolasmorlot@gmail.com>
2019-11-15 13:11:38 +0100

    nav bar et photo, problem lien home.. faire ticket

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   resources :event_users, only: [:index, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_scope :user do
+  #   get 'log_in', to: 'events#home'
+  # end
+
 end
 
 
