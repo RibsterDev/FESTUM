0c0b91209ec977e44bf8ea2aea470962ad50d309
jeremy gibilaro <jeremygibilaro@gmail.com>
2019-11-15 15:06:11 +0100

    resolution du bugg typo

--- app/javascript/packs/typed_js.js
+++ app/javascript/packs/typed_js.js
@@ -1,6 +1,9 @@
 import Typed from 'typed.js';
 
 const dynamicAddEvent = () => {
+  if (document.querySelector('#type-script') == null)
+    return
+
   new Typed('#type-script', {
     strings: ["Borring with your life...", "It's time to do the Show !"],
     typeSpeed: 50,
